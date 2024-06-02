7553600e3065eab0245307dd071aa2607180d1af
rixiobarrios <rixiobarrios.consulado@gmail.com>
2024-06-02 15:47:25 -0500

    third commit

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,29 +1,3 @@
-// const expect = require('fix-esm').require('chai').expect;
-// const request = require('fix-esm').require('request');
-// const fetch = require('node-fetch');
-// const assert = require('fix-esm').require('assert');
-
-// it('Main page content', function (done) {
-//     request('http://localhost:3000', function (err, res, body) {
-//         // assert.equal(body, undefined);
-//         // assert.equal(body, 'Hello World');
-//         expect(body).to.equal('Hello World');
-//         done();
-//     });
-// });
-
-// const chai = require('chai');
-// const expect = chai.expect;
-// const fetch = require('node-fetch');
-
-// describe('Main page content', function () {
-//     it('should respond with Hello World', async function () {
-//         const response = await fetch('http://localhost:3000');
-//         const body = await response.text();
-//         expect(body).to.equal('Hello World');
-//     });
-// });
-
 // Import chai and expect
 import { expect } from 'chai';
 // Import node-fetch
