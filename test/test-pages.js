e6894e9eed6b4cbe6f49310c3400200f1b4c9d9e
rixiobarrios <rixiobarrios.consulado@gmail.com>
2024-06-02 17:03:29 -0500

    fifth commit

--- test/test-pages.js
+++ test/test-pages.js
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 import fetch from 'node-fetch';
 
 describe('Main page content', () => {
-    it('should respond with Hello World', async () => {
+    it('Should respond with Hello World', async () => {
         const response = await fetch('http://localhost:3000');
         const body = await response.text(); // Convert response to text
         expect(body).to.equal('Hello World');
@@ -12,8 +12,8 @@ describe('Main page content', () => {
 });
 
 describe('HTTP Request Tests', () => {
-    it('Main page status', async () => {
-        const response = await fetch('http://localhost:8080');
+    it('Main page status should be 200', async () => {
+        const response = await fetch('http://localhost:3000');
         const statusCode = response.status;
 
         expect(statusCode).to.equal(200);
@@ -21,8 +21,8 @@ describe('HTTP Request Tests', () => {
 });
 
 describe('HTTP Request Tests', () => {
-    it('About page content', async () => {
-        const response = await fetch('http://localhost:8080/about');
+    it('About page content should be 404', async () => {
+        const response = await fetch('http://localhost:3000/about');
         const statusCode = response.status;
 
         expect(statusCode).to.equal(404);
